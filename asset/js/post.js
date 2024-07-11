ed69cab30657b10cc82f004c849c8b86f8312de1
fajryalvin12 <fajryalvin12@gmail.com>
2024-07-11 09:13:59 +0700

    show table data from url https://st2lww-8888.csb.app/fajry/data in the page using async await function

--- asset/js/post.js
+++ asset/js/post.js
@@ -1,21 +1,50 @@
 const form = document.getElementById("form-survey");
-
 const tableBody = document.getElementById("insert-data");
-
 const endpoint = "https://st2lww-8888.csb.app/fajry/data";
 
-form.addEventListener("submit", processData);
+async function insertTable() {
+  const data = await fetch(endpoint);
+  const dataTable = await data.json();
+  tableBody.innerHTML = "";
 
-function processData(event) {
-  event.preventDefault();
-  const inputName = event.target.name.value;
-  const inputAge = event.target.age.value;
-  const inputGender = event.target.gender.value;
-  const inputIsSmoke = event.target.ask.value;
-  console.log(inputName);
-  console.log(inputAge);
-  console.log(inputGender);
-  console.log(inputIsSmoke);
+  dataTable.results.forEach((e) => {
+    const tableRow = document.createElement("tr");
+    const tableName = document.createElement("td");
+    const tableAge = document.createElement("td");
+    const tableGender = document.createElement("td");
+    const tableSmoke = document.createElement("td");
+    const tableVariant = document.createElement("td");
+    tableName.innerHTML = e.name;
+    tableAge.innerHTML = e.age;
+    tableGender.innerHTML = e.gender;
+    tableVariant.innerHTML = e.cigarVariant.join("; ");
+
+    tableRow.appendChild(tableName);
+    tableRow.appendChild(tableAge);
+    tableRow.appendChild(tableGender);
+    if (e.isSmoker) {
+      tableSmoke.innerHTML = "Ya";
+      tableRow.appendChild(tableSmoke);
+    } else {
+      tableSmoke.innerHTML = "Tidak";
+      tableRow.appendChild(tableSmoke);
+    }
+    tableRow.appendChild(tableVariant);
+    tableBody.appendChild(tableRow);
+  });
+}
+
+insertTable();
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const inputName = e.target.name.value;
+  const inputAge = e.target.age.value;
+  const inputGender = e.target.gender.value;
+  const inputIsSmoke = e.target.ask.value;
+  // console.log(inputName);
+  // console.log(inputAge);
+  // console.log(inputGender);
+  // console.log(inputIsSmoke);
 
   const cigar = document.getElementsByName("smoke");
   let arrCigar = [];
@@ -24,7 +53,7 @@ function processData(event) {
       arrCigar.push(cigar[i].value);
     }
   }
-  console.log(arrCigar.join("; "));
+  // console.log(arrCigar.join("; "));
 
   const dataForm = new URLSearchParams();
   dataForm.append("name", inputName);
@@ -33,37 +62,27 @@ function processData(event) {
   dataForm.append("isSmoker", inputIsSmoke);
   dataForm.append("cigarVariant", arrCigar);
 
-  fetch(endpoint, {
+  const askData = await fetch(endpoint, {
     method: "POST",
     body: dataForm,
-  }).then((response) => {
-    response.json().then((data) => {
-      if (data.success) {
-        window.alert("Data disimpan");
-      } else {
-        window.alert(data.message);
-      }
-    });
   });
-}
-
-async function restAPI() {
-  const tableRow = document.createElement("tr");
-  const tableName = document.createElement("td");
-  const tableAge = document.createElement("td");
-  const tableGender = document.createElement("td");
-  const tableSmoke = document.createElement("td");
-  const tableVariant = document.createElement("td");
-
-  const checkName = await fetch(endpoint);
-  const data = await endpoint.json();
-  for (let i = 0; i < data.length; i++) {
-    tableRow.appendChild(tableAge);
-    tableRow.appendChild(tableGender);
-    tableRow.appendChild(tableSmoke);
-    tableRow.appendChild(tableVariant);
-    tableBody.appendChild(tableRow);
+  const data = await askData.json();
+  if (data.success) {
+    window.alert(data.message);
+  } else {
+    window.alert("mohon maaf, data anda salah cok");
   }
-}
-
-restAPI();
+  insertTable();
+});
+// fetch(endpoint, {
+//   method: "POST",
+//   body: dataForm,
+// }).then((response) => {
+//   response.json().then((data) => {
+//     if (data.success) {
+//       window.alert("Data disimpan");
+//     } else {
+//       window.alert(data.message);
+//     }
+//   });
+// });
